a453b624b9854fa2d899a1cb00c47c4f29588b2f
Ryan W <ryanweavers@gmail.com>
2025-06-25 21:41:25 +0100

    feat(scrape table): scraping the basketball table for the first time, output could be improved

---
+++ WebScraper-RyanW84/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata;
+
+using HtmlAgilityPack;
+
+namespace WebScraper_RyanW84
+{
+    public class WebScraper
+    {
+        public static async Task Main(string[] args)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load("https://www.basketball-reference.com/boxscores/");
+
+            var title = document.DocumentNode.SelectNodes("//div/h1").First().InnerText;
+            var tableoutput = document.DocumentNode.SelectNodes("//*[@id=\"confs_standings_E\"]/tbody/tr");
+
+			Console.WriteLine(title);
+			Console.WriteLine();
+			tableoutput.ToList().ForEach(x => Console.WriteLine(x.InnerText));
+
+        }
+    }
+}
